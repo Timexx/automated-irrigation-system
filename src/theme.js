6861aadcc0be2b2d53af8d83538a517d45ddab6d
Tim <tim.pollmer@gmail.com>
2022-01-07 19:51:10 +0100

    farben angepasst für besseren Kontrast zu Cards

--- src/theme.js
+++ src/theme.js
@@ -2,19 +2,19 @@ const theme = {
   colors: {
     text: "white",
     background: "#161A30",
-    backgroundSecondary: "#262F4D",
+    backgroundSecondary: "#FAFAFA",
     muted: "#fff",
     modes: {
       light: {
         text: "#262F4D",
-        background: "white",
+        background: "#FAFAFA",
         backgroundSecondary: "white",
         muted: "#444"
       },
       dark: {
         text: "white",
         background: "#161A30",
-        backgroundSecondary: "#262F4D",
+        backgroundSecondary: "#FAFAFA",
         muted: "#fff"
       }
     }
