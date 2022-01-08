e738273105f3d38c1bbd1ab2160e7bdc545312ad
Tim <tim.pollmer@gmail.com>
2022-01-08 10:40:16 +0100

    change colors and Font wight

--- src/theme.js
+++ src/theme.js
@@ -2,13 +2,13 @@ const theme = {
   colors: {
     text: "white",
     background: "#161A30",
-    backgroundSecondary: "#F5F5F5",
+    backgroundSecondary: "#EFF1EC",
     muted: "#fff",
     modes: {
       light: {
         text: "#262F4D",
-        background: "#FAFAFA",
-        backgroundSecondary: "F5F5F5",
+        background: "#FBFDF8",
+        backgroundSecondary: "EFF1EC",
         muted: "#444"
       },
       dark: {
